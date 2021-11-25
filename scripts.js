3a211d8b22cce2ff851ea00f58fbd63378f7554b
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-11-25 19:21:37 -0300

    answer display

--- scripts.js
+++ scripts.js
@@ -11,18 +11,26 @@ function shuffle(array) {
   return array;
 }
 a = shuffle(a);
-console.log(a)
+
+let txt_resposta = document.getElementById("resposta");
 let cont = 0;
 let botao_pergunta = document.getElementById("nextButton");
 botao_pergunta.onclick = () => {
+	txt_resposta.style.display = "none";
     let pergunta = ["POR QUANTO TEMPO DEVEMOS LAVAR AS MÃOS ANTES DE ASSUMIR UMA ESTAÇÃO?", "QUAIS OS QUATRO FUNDAMENTOS BÁSICOS DO MC DONALD’S (QSLV)?", "QUAL A TEMPERATURA DA TOSTADEIRA (PRINCIPAL/AUXILIAR) ?"];
     let resposta = ["20 SEGUNDOS", "QUALIDADE, SERVIÇO, LIMPEZA E VALOR", "TOSTADEIRA PRINCIPAL 293°C E AUXILIAR 204°"];
 
-    let text_pergunta = document.getElementById("resposta");
-    let text_resposta = document.getElementById("texto");
+    let text_pergunta = document.getElementById("texto");
+    let text_resposta = document.getElementById("resposta");
 
 
     text_pergunta.innerHTML = pergunta[a[cont]];
     text_resposta .innerHTML =  resposta[a[cont]];
     cont++;
+
+}
+
+let botao_resposta = document.getElementById("answer");
+botao_resposta.onclick = () => {
+	txt_resposta.style.display = "block";
 }
